7429615130b812749608a86cffce134e1e5f8069
Nicolas Carrillo <nico@abeinc.net>
2026-09-01 22:20:34 -0600

    Aplicacion piedra papel o tijera

---
+++ piedra_papel_tijera.py
@@ -0,0 +1,25 @@
+import random
+
+opciones = ["piedra", "papel", "tijera"]
+
+jugador = input("\nElije piedra, papel o tijera ").lower()
+
+while jugador not in opciones:
+    print("\nOpcion no valida")
+    jugador = input("\nElije piedra, papel o tijera ").lower()
+    
+computadora = random.choice(opciones)
+
+print(f"Computadora: {computadora}")
+
+if jugador == computadora:
+    print("\nEmpate")
+
+elif(jugador == "piedra" and computadora == "tijera") or\
+    (jugador == "papel" and computadora == "piedra") or\
+    (jugador == "tijera" and computadora == "papel"):
+        print("\nGanaste!")
+        
+else:
+    print("\nPerdiste")
+    
