1f952ed0bae244024149dafdcc9c97ec8f93610a
Nicolas Carrillo <nico@abeinc.net>
2026-09-03 20:04:00 -0600

    Actualizacion con pregunta para salir

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -1,25 +1,50 @@
 import random
 
-opciones = ["piedra", "papel", "tijera"]
+juga = 0
+compu = 0
+empate = 0
 
-jugador = input("\nElije piedra, papel o tijera ").lower()
-
-while jugador not in opciones:
-    print("\nOpcion no valida")
-    jugador = input("\nElije piedra, papel o tijera ").lower()
+while True:
     
-computadora = random.choice(opciones)
-
-print(f"\nComputadora: {computadora}")
-
-if jugador == computadora:
-    print("\nEmpate")
-
-elif(jugador == "piedra" and computadora == "tijera") or\
-    (jugador == "papel" and computadora == "piedra") or\
-    (jugador == "tijera" and computadora == "papel"):
-        print("\nGanaste!")
+    opciones = ["piedra", "papel", "tijera"]
         
-else:
-    print("\nPerdiste")
+    jugador = input("\nElije piedra, papel o tijera ").lower()
+
+    while jugador not in opciones:
+        print("\nOpcion no valida")
+        jugador = input("\nElije piedra, papel o tijera ").lower()
+        
+    computadora = random.choice(opciones)
+
+    print(f"\nComputadora: {computadora}")
+
+
+    if jugador == computadora:
+        print("\nEmpate")
+        
+        empate += 1
+        
+        print(f"Marcador: Jugador {juga} -- Computadora {compu} -- Empate {empate}")
+
+    elif(jugador == "piedra" and computadora == "tijera") or\
+        (jugador == "papel" and computadora == "piedra") or\
+        (jugador == "tijera" and computadora == "papel"):
+            print("\nGanaste!")
+                
+            juga += 1
+            
+            print(f"Marcador: Jugador {juga} -- Computadora {compu} -- Empate {empate}")
+            
+    else:
+        print("\nPerdiste")
+        
+        compu += 1
+        
+        print(f"Marcador: Jugador {juga} -- Computadora {compu} -- Empate {empate}")
+        
+        continuar = input("\nQuieres seguir jugando (s/n): ").lower()
+        
+        if continuar != "s":
+            print("\nGracias por jugar")
+            break
     
