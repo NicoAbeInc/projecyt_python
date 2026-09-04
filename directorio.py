c30612bd51af25e4e294185469619fafbabf6d24
Nicolas Carrillo <nico@abeinc.net>
2026-09-03 22:54:35 -0600

    Directorio tarea punto 4

---
+++ directorio.py
@@ -0,0 +1,44 @@
+print("--- DIRECTORIO ---")
+
+contactos = [
+    {"id": 1, "nombre": "Marco", "telefono": "2871104293", "email": "marco@hotmail.com"},
+    {"id": 2, "nombre": "Ana", "telefono": "2871122343", "email": "ana@hotmail.com"}
+]
+
+while True:
+
+    for contacto in contactos:
+        print(f"\n{contacto["nombre"]} {contacto["telefono"]} {contacto["email"]}")
+        
+    print("\nOpciones")
+    print("1. Buscar")
+    print("2. Agregar")
+    print("3. Elimnar")
+    print("4. Salir")
+
+    opcion = int(input("\nElige una opcion: "))
+
+    if opcion == 1:
+        nom = input("\nIngresa el nombre: ")
+        for contacto in contactos:
+            if contacto["nombre"] == nom:
+                print(f"\n{contacto["nombre"]} {contacto["telefono"]} {contacto["email"]}")
+                print("-------------")
+            
+    elif opcion == 2:
+        id_ult = contactos[-1]["id"] + 1
+        nom = input("Nombre: ")
+        tel= int(input("Telefono: "))
+        ema = input("Email: ")
+        
+        contactos.append({"id": id_ult, "nombre": nom, "telefono": tel, "email": ema})
+        
+    elif opcion == 3:
+        elim = input("Contacto a eliminar: ")
+        for contacto in contactos:
+            if contacto["nombre"] == elim:
+                contactos.remove(contacto)
+                break
+        
+    elif opcion == 4:
+        break
